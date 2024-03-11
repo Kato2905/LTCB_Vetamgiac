0999696d26f71632befa8a531f16a9db3ddc604c
ADMIN <ADMIN@LAB1-MAY29>
2024-03-11 09:45:29 +0700

    Add project files.

---
+++ LTCB_Vetamgiac/LTCB_Vetamgiac.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int so; cout << "Nhap so:"; cin >> so;
+	for (int i = 0; i < so; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
+
+/*int main()
+{
+	int so; cout << "Nhap so"; cin >> so;342
+	for (int i = 0; i < so; i++) {
+		for (int j = so; j > i; j--) {
+			cout << "* ";
+		}
+		cout << endl;
+	}
+	return 0;
+}*/
+ 
